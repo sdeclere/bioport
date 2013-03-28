caf6d90a8bebe44c196cb29c4be79da7ece86ee8
Stephane Declere <stephane.descorps-declere@pasteur.fr>
2013-03-28 17:30:36 +0100

    ADD : zip target / sickle mk

---
+++ makefiles/sickle.mk
@@ -0,0 +1,18 @@
+DISTNAME = sickle
+VERSION = 1_2
+
+URL = https://github.com/najoshi/sickle/archive/master.zip
+ARCHIVE = master.zip
+
+BUILD_SCRIPTS = $(WORKSRC)/sickle-master/Makefile
+CLEAN_SCRIPTS = $(WORKSRC)/sickle-master/Makefile
+
+include ports.mk
+
+WGET_OPTS += --no-check-certificate
+LDFLAGS += -lz
+
+post-build: 
+	@$(INSTALL) -C $(WORKSRC)/sickle-master/sickle $(PORTSDIR)/bin/
+	
+
